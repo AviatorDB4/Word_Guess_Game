05e9f330bb38a117aa1c418d8cadcba9491e4a77
David Barton <david@Davids-MacBook-Pro.local>
2018-08-12 17:13:46 -0700

    more changes

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -1,6 +1,6 @@
 object.onload = function initializeGame(){
 
-
+var initializeGame;
 var baseballTerm = ['pastime', 'American League', 'National League', 'World Series', 'strike', 'ball', 'out','safe', 'homerun', 'Diamondbacks', 'Braves', 'Orioles', 'Boston Red Sox', 'Chicago Cubs', 'Chicago White Sox', 'Cincinnati Reds', 'Indians', 'Rockies', 'Tigers', 'Astros', 'Royals', 'LA Angels', 'LA Dodgers', 'Maimi Marlins', 'Brewers', 'Twins', 'NY Mets', 'NY Yankees', 'Athletics', 'Phillies', 'Pirates', 'Gaints', 'Mariners', 'Cardinals', 'TB Rays', 'Rangers', 'Blue Jays', 'Nationals'];
 var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z'];
 var userGuess;
@@ -10,7 +10,7 @@ var allowedGuess;
 var mylives = 10;
 var updateGuess;
 
-
+var initializeGame = document.getElementById('initializeGame')
 var updateGuess = document.getElementById('updateGuess');
 var allowedGuess = document.getElementById('allowedGuess');
 var correctGuess = document.getElementById('correctGuess');
